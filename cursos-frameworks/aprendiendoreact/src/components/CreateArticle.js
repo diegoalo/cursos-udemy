1bb0e73156f5046749a9d6108d360c28f8a907fe
Diego Alonso <diego.alonso.meg@gmail.com>
2020-09-02 17:06:35 +0200

    React: 202/262

--- cursos-frameworks/aprendiendoreact/src/components/CreateArticle.js
+++ cursos-frameworks/aprendiendoreact/src/components/CreateArticle.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 import Axios from 'axios';
 import Global from '../Global';
 import Sidebar from './Sidebar';
+import Slider from './Slider';
 
 //validación formularios y alertas
 
@@ -17,12 +18,13 @@ class CreateArticle extends Component {
     //creamos el estado para guardar la informacion
     state = {
         article: {},
-        status: null
+        status: null,
+        selectedFile: null
     };
 
     changeState = () => {
         this.setState({
-            article:{
+            article: {
                 title: this.titleRef.current.value,
                 content: this.contentRef.current.value,
             }
@@ -40,64 +42,126 @@ class CreateArticle extends Component {
 
         //Hacemos una peticion HTTP por POST para guardar el articulo
         Axios.post(this.url + 'save', this.state.article)
-        .then( res => {
-            //en caso de que llegue la respuesta del articulo es que todo ha ido bien
-            if(res.data.article){
-                this.setState({
-                    article: res.data.article,
-                    status: 'success'
-                });
-            }else {
-                this.setState({
-                    //en caso contrario devolvemos un error
-                    status: 'failed'
-                });
-            }
-        })
-        
+            .then(res => {
+                //en caso de que llegue la respuesta del articulo es que todo ha ido bien
+                if (res.data.article) {
+                    this.setState({
+                        article: res.data.article,
+                        status: 'waiting'
+                    });
+
+                    //subir el archivo (la imagen)
+                    if (this.state.selectedFile !== null){
+
+                        //sacamos el id del articulo guardado
+                        var articleId = this.state.article._id;
+
+                        //crear form data y añadir el fichero
+                        const formData = new FormData();
+                        //vincular un fichero al form
+                        formData.append(
+                            'file0',
+                            this.state.selectedFile,
+                            this.state.selectedFile.name
+
+                        );
+
+                        //peticion AJAX
+                        Axios.post(this.url + 'upload-image/' + articleId, formData)
+                        .then(res => {
+                            //la imagen se sube correctamente
+                            if(res.data.article){
+                                this.setState({
+                                    article: res.data.article,
+                                    status: 'success'
+                                });
+                            }else{
+                                this.setState({
+                                    article: res.data.article,
+                                    status: 'failed'
+                                });
+                            }
+                        });
+
+                    }else{
+                        this.setState({
+                            status: 'success'
+                        });
+                    }
+
+
+                } else {
+                    this.setState({
+                        //en caso contrario devolvemos un error
+                        status: 'failed'
+                    });
+                }
+            })
+
+    }
+
+    //metodo para la subida de imagenes
+    fileChange = (event) => {
+        //actualizamos el state seleccionando el archivo que seleccionamos (Viene en un array, seria el primero)
+        this.setState({
+            selectedFile: event.target.files[0]
+        });
+
+        console.log(this.state);
     }
 
     render() {
 
         //si ha ido bien, redirigimos al blog para ver el articulo nuevo
-        if(this.state.status === "success"){
+        if (this.state.status === "success") {
             return <Redirect to="/blog" />;
         }
 
         return (
-            <div className="center">
 
-                <section id="contact">
+            <React.Fragment>
 
-                    <h1 className="subheader">Crear artículo</h1>
+                <Slider
+                    title="Nuevo artículo"
+                    size="slider-small"
+                />
+                <div className="center">
 
-                    {/* Formulario */}
-                    <form className="mid-form" onSubmit={this.saveArticle}>
-                        <div className="form-group">
-                            <label htmlFor="title">Título</label>
-                            <input type="text" name="title" ref={this.titleRef} onChange={this.changeState} />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="content">Contenido</label>
-                            <textarea name="content" ref={this.contentRef} onChange={this.changeState} />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="file0">Imagen</label>
-                            <input type="file" name="file0" />
-                        </div>
-
-                        <input type="submit" value="Guardar" className="btn-success" />
+                    <section id="contact">
 
 
-                    </form>
+                        <h1 className="subheader">Rellena los datos</h1>
 
-                </section>
+                        {/* Formulario */}
+                        <form className="mid-form" onSubmit={this.saveArticle}>
+                            <div className="form-group">
+                                <label htmlFor="title">Título</label>
+                                <input type="text" name="title" ref={this.titleRef} onChange={this.changeState} />
+                            </div>
 
-                <Sidebar />
+                            <div className="form-group">
+                                <label htmlFor="content">Contenido</label>
+                                <textarea name="content" ref={this.contentRef} onChange={this.changeState} />
+                            </div>
 
-            </div>
+                            <div className="form-group">
+                                <label htmlFor="file0">Imagen</label>
+                                <input type="file" name="file0" onChange={this.fileChange}/>
+                            </div>
+
+                            <input type="submit" value="Guardar" className="btn-success" />
+
+
+                        </form>
+
+                    </section>
+
+                    <Sidebar />
+
+
+                </div>
+
+            </React.Fragment>
         );
     }
 }
