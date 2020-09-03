ed4462a50e36f82ce4884a74fcc2ec18bdd7392d
Diego Alonso <diego.alonso.meg@gmail.com>
2020-09-03 21:44:21 +0200

    React: 204/262

--- cursos-frameworks/aprendiendoreact/src/components/CreateArticle.js
+++ cursos-frameworks/aprendiendoreact/src/components/CreateArticle.js
@@ -4,6 +4,10 @@ import Axios from 'axios';
 import Global from '../Global';
 import Sidebar from './Sidebar';
 import Slider from './Slider';
+//validar formularios
+import SimpleReactValidator from 'simple-react-validator';
+//libreria para alertas en React
+import swal from 'sweetalert';
 
 //validación formularios y alertas
 
@@ -22,6 +26,16 @@ class CreateArticle extends Component {
         selectedFile: null
     };
 
+    //cargamos el validador de formularios de React
+    componentWillMount() {
+        this.validator = new SimpleReactValidator({
+            //configuramos los errores que queremos mostrar
+            messages: {
+                required: 'Este campo es requerido'
+
+            }});
+    }
+
     changeState = () => {
         this.setState({
             article: {
@@ -29,6 +43,10 @@ class CreateArticle extends Component {
                 content: this.contentRef.current.value,
             }
         });
+
+        //mostramos mensajes de error del formulario
+        this.validator.showMessages();
+        this.forceUpdate();
 
     }
 
@@ -40,65 +58,86 @@ class CreateArticle extends Component {
         //Rellenamos el state con el formulario
         this.changeState();
 
-        //Hacemos una peticion HTTP por POST para guardar el articulo
-        Axios.post(this.url + 'save', this.state.article)
-            .then(res => {
-                //en caso de que llegue la respuesta del articulo es que todo ha ido bien
-                if (res.data.article) {
-                    this.setState({
-                        article: res.data.article,
-                        status: 'waiting'
-                    });
 
-                    //subir el archivo (la imagen)
-                    if (this.state.selectedFile !== null){
+        if (this.validator.allValid()) {
+            Axios.post(this.url + 'save', this.state.article)
+                .then(res => {
+                    //en caso de que llegue la respuesta del articulo es que todo ha ido bien
+                    if (res.data.article) {
+                        this.setState({
+                            article: res.data.article,
+                            status: 'waiting'
+                        });
 
-                        //sacamos el id del articulo guardado
-                        var articleId = this.state.article._id;
-
-                        //crear form data y añadir el fichero
-                        const formData = new FormData();
-                        //vincular un fichero al form
-                        formData.append(
-                            'file0',
-                            this.state.selectedFile,
-                            this.state.selectedFile.name
-
+                        swal(
+                            'Artículo creado!',
+                            'El artículo se ha incluido correctamente',
+                            'success'
                         );
 
-                        //peticion AJAX
-                        Axios.post(this.url + 'upload-image/' + articleId, formData)
-                        .then(res => {
-                            //la imagen se sube correctamente
-                            if(res.data.article){
-                                this.setState({
-                                    article: res.data.article,
-                                    status: 'success'
-                                });
-                            }else{
-                                this.setState({
-                                    article: res.data.article,
-                                    status: 'failed'
-                                });
-                            }
-                        });
+                        //subir el archivo (la imagen)
+                        if (this.state.selectedFile !== null) {
 
-                    }else{
+                            //sacamos el id del articulo guardado
+                            var articleId = this.state.article._id;
+
+                            //crear form data y añadir el fichero
+                            const formData = new FormData();
+                            //vincular un fichero al form
+                            formData.append(
+                                'file0',
+                                this.state.selectedFile,
+                                this.state.selectedFile.name
+
+                            );
+
+                            //peticion AJAX
+                            Axios.post(this.url + 'upload-image/' + articleId, formData)
+                                .then(res => {
+                                    //la imagen se sube correctamente
+                                    if (res.data.article) {
+                                        this.setState({
+                                            article: res.data.article,
+                                            status: 'success'
+                                        });
+                                    } else {
+                                        this.setState({
+                                            article: res.data.article,
+                                            status: 'failed'
+                                        });
+                                    }
+                                });
+
+                        } else {
+                            this.setState({
+                                status: 'success'
+                            });
+                        }
+
+
+                    } else {
                         this.setState({
-                            status: 'success'
+                            //en caso contrario devolvemos un error
+                            status: 'failed'
                         });
                     }
+                })
 
+        }else{
 
-                } else {
-                    this.setState({
-                        //en caso contrario devolvemos un error
-                        status: 'failed'
-                    });
-                }
-            })
+            //el estado se marca a fallido
+            this.setState({
+                //en caso contrario devolvemos un error
+                status: 'failed'
+            });
 
+            //si la validacion del formulario no ha sido correcta, se muestran los mensajes de error
+            this.validator.showMessages();
+            this.forceUpdate();
+        }
     }
+    //Hacemos una peticion HTTP por POST para guardar el articulo
+
 
     //metodo para la subida de imagenes
     fileChange = (event) => {
@@ -137,16 +176,21 @@ class CreateArticle extends Component {
                             <div className="form-group">
                                 <label htmlFor="title">Título</label>
                                 <input type="text" name="title" ref={this.titleRef} onChange={this.changeState} />
+
+                                {this.validator.message('title', this.state.article.title, 'required|alpha_num_space')}
+
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="content">Contenido</label>
                                 <textarea name="content" ref={this.contentRef} onChange={this.changeState} />
+
+                                {this.validator.message('content', this.state.article.content, 'required')}
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="file0">Imagen</label>
-                                <input type="file" name="file0" onChange={this.fileChange}/>
+                                <input type="file" name="file0" onChange={this.fileChange} />
                             </div>
 
                             <input type="submit" value="Guardar" className="btn-success" />
