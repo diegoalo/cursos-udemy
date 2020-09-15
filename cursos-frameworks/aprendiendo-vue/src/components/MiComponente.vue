23e665720cf8ec88826806aa4d9d2ab07c1a5027
Diego Alonso <diego.alonso.meg@gmail.com>
2020-09-15 22:03:48 +0200

    Vue: 225/262

--- cursos-frameworks/aprendiendo-vue/src/components/MiComponente.vue
+++ cursos-frameworks/aprendiendo-vue/src/components/MiComponente.vue
@@ -1,15 +1,25 @@
 
 <template>
-<section id="content">
+  <section id="content">
     <h2 class="subheader">{{titulo}}</h2>
-<!-- template donde se escribe el HTML para el componente  -->
-  <div id ="mi-componente">
-    <hr/>
-    <h1>Este es mi componente</h1>
-    <h2>{{subtitulo}}</h2>
-    <hr/>
-  </div>
-</section>
+    <!-- template donde se escribe el HTML para el componente  -->
+    <div id="mi-componente">
+      <hr />
+      <h1>Este es mi componente</h1>
+      <h2>{{subtitulo}}</h2>
+      <hr />
+
+      <h2>Reactividad</h2>
+      <input type="text" v-model="nombre" />
+      Mi nombre es
+      <strong>{{nombre}}</strong>
+    </div>
+
+    <!--  mostrar los datos <pre>
+      {{$data}}
+    </pre>-->
+
+  </section>
 </template>
 
 <script>
@@ -19,8 +29,12 @@ export default {
   name: "MiComponente",
   data() {
     return {
-      titulo: 'Mi Componente',
+      titulo: "Mi Componente",
       subtitulo: "Soy el subtitulo de MiComponente",
+      //directiva: da funcionalidad a elementos del HTML
+      //al hacer el binding con la propiedad nombre con la directiva v-model, se cambia la propiedad
+      //directamente desde el formulario
+      nombre: "",
     };
   },
 };
